55f1ae99b69e2058e948232f4a9d948335c3fbb6
Fadhil Radhian <fadhil.radhian@wartek.belajar.id>
2022-06-14 10:59:15 +0700

    add module and several functions

--- db/query/account.sql
+++ db/query/account.sql
@@ -1,4 +1,4 @@
--- name: CreateAuthor :one
+-- name: CreateAccount :one
 INSERT INTO account (
   owner, 
   balance, 
@@ -7,3 +7,22 @@ INSERT INTO account (
   $1, $2, $3
 )
 RETURNING *;
+
+-- name: GetAccountByID :one
+SELECT * FROM account
+WHERE id = $1 LIMIT 1;
+
+-- name: GetAccounts :many
+SELECT * FROM account
+ORDER BY name
+LIMIT $1
+OFFSET $2;
+
+-- name: UpdateAccount :one
+UPDATE account SET balance = $2
+WHERE id = $1
+RETURNING *;
+
+-- name: DeleteAccount :exec
+DELETE FROM account
+WHERE id = $1;
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/fadhilradh/simplebank
+
+go 1.18
